19ca86642b6b47821d08a575d78f0d9201f29220
AdriZ <adrizavi-94@hotmail.com>
2013-10-30 16:20:35 +0100

    programa jefe

---
+++ test.py
@@ -0,0 +1,8 @@
+import DecimalBinario
+
+numero = int(raw_input("Introduce el numero decimal a convertir a binario: "))
+print (DecimalBinario.Decimal2Binario(numero))
+
+str = raw_input ("Introduce el numero binario a convertir a decimal: ")
+
+print (DecimalBinario.Binario2Decimal(str))
